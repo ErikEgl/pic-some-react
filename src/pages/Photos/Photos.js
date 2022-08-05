b8bd6163bbb1ee4e6831ac8d4975be3ba1b647db
Erik <Mr.erik.egliens@gmail.com>
2022-08-05 14:06:55 +0300

    added react router

--- src/pages/Photos/Photos.js
+++ src/pages/Photos/Photos.js
@@ -1,11 +1,13 @@
-import React from "react"
+import React from "react";
 
 function Photos() {
-    return (
-        <main className="photos">
-            <h1>Images go here</h1>
-        </main>
-    )
+  return (
+    <>
+      <main className="photos">
+        <h1>Images go here</h1>
+      </main>
+    </>
+  );
 }
 
-export default Photos
+export default Photos;
