7a07423a65a40c2699676a88229612326a32be75
Erik <Mr.erik.egliens@gmail.com>
2022-08-06 23:12:42 +0300

    image component refactoring

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,16 +1,32 @@
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import { UserContext } from "../../utils/useContext";
+function Image({className, img}) {
+  const [hovered, setHovered] = React.useState(false);
+  const {toggleFavorite} = useContext(UserContext);
   function onEnter() {
-    setHovered(prevState => !prevState)
+    setHovered((prevState) => !prevState);
   }
   function onLeave() {
-    setHovered(prevState => !prevState)
+    setHovered((prevState) => !prevState);
   }
 
   return (
     <>
-      <div onMouseEnter={onEnter} onMouseLeave={onLeave}  className={`${props.className} image-container`}>
-        <img src={props.img} alt="pic" className="image-grid" />
-        {(hovered || props.isFavorite) && <i onClick={() => props.toggle(props.id)} className={`${props.isFavorite ? 'ri-heart-fill' : 'ri-heart-line'} favorite`}></i>}
+      <div
+        onMouseEnter={onEnter}
+        onMouseLeave={onLeave}
+        className={`${className} image-container`}
+      >
+        <img src={img.url} alt="pic" className="image-grid" />
+        {(hovered || img.isFavorite) && (
+          <i
+            onClick={() => toggleFavorite(img.id)}
+            className={`${
+              img.isFavorite ? "ri-heart-fill" : "ri-heart-line"
+            } favorite`}
+          ></i>
+        )}
         {hovered && <i className="ri-add-circle-line cart"></i>}
       </div>
     </>
