08da4debe9fa058717eaec5c2fa5a380a08b95d2
Erik <Mr.erik.egliens@gmail.com>
2022-08-06 08:48:59 +0300

    hover state

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,9 +1,21 @@
 import React from "react";
 function Image(props) {
+
+  const [hovered, setHovered] = React.useState(false)
+
+  function onEnter() {
+    setHovered(prevState => !prevState)
+  }
+  function onLeave() {
+    setHovered(prevState => !prevState)
+  }
+
   return (
     <>
-      <div className={`${props.className} image-container`}>
+      <div onMouseEnter={onEnter} onMouseLeave={onLeave}  className={`${props.className} image-container`}>
         <img src={props.img} alt="pic" className="image-grid" />
+        {hovered && <i className="ri-heart-line favorite"></i>}
+        {hovered && <i className="ri-add-circle-line cart"></i>}
       </div>
     </>
   );
