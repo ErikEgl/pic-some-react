91286305fc6159d0d9bcd9ef1b7cae0fbf5531d7
Erik <Mr.erik.egliens@gmail.com>
2022-08-07 21:20:14 +0300

    cart icon styles on hover

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,12 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../../utils/useContext";
 
 function CartItem({item, price}) {
     const {toggleCartItem} = useContext(UserContext);
+    const [hovered, setHovered] = useState(false);
+    function onEnter() {
+        setHovered(true);
+      }
+      function onLeave() {
+        setHovered(false);
+      }
 
     return (
         <div className="cart-item">
-            <i  onClick={() => toggleCartItem(item)} className="ri-delete-bin-line"></i>
+            <i 
+            onMouseEnter={onEnter}
+            onMouseLeave={onLeave}
+            onClick={() => toggleCartItem(item)} className={hovered ? `ri-delete-bin-fill` : `ri-delete-bin-line`}></i>
             <img alt={`Cart Item ${item.id}`}  src={item.url} width="130px" />
             <p>{price.toLocaleString("en-US", {style: "currency", currency: "USD"})}</p>
         </div>
