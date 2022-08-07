da7de9fd37802391978df9ce5a3f9db05d4abcde
Erik <Mr.erik.egliens@gmail.com>
2022-08-07 15:02:49 +0300

    cart item component

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,16 +1,24 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../utils/useContext";
+import CartItem from "../../components/CartItem/CartItem";
 
 function Cart() {
-  // const context = useContext(UserContext);
-  return (
-    <>
-      <main className="cart-page">
-        <h1>Check out</h1>
-        {/* <h1>{context}</h1> */}
-      </main>
-    </>
-  );
+  const {cartItems} = useContext(UserContext);
+
+    const cartItemElements = cartItems.map(item => (
+        <CartItem key={item.id} item={item} />
+    ))
+    
+    return (
+        <main className="cart-page">
+            <h1>Check out</h1>
+            {cartItemElements}
+            <p className="total-cost">Total: </p>
+            <div className="order-button">
+                <button>Place Order</button>
+            </div>
+        </main>
+    )
 }
 
-export default Cart;
+export default Cart
