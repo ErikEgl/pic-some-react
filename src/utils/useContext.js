45925c48507e5b158f677db544f72add52a321a6
Erik <Mr.erik.egliens@gmail.com>
2022-08-07 10:29:10 +0300

    code prettier

--- src/utils/useContext.js
+++ src/utils/useContext.js
@@ -2,45 +2,45 @@ import { useState, useEffect, createContext } from "react";
 const UserContext = createContext();
 
 function AppContextProvider(props) {
-    const [photos, setPhotos] = useState([])
-    const [cartItems, setCartItems] = useState([])
+  const [photos, setPhotos] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
-    useEffect(() => {
-        const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
-        fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            setPhotos(data)
-        });
-    }, [])
+  useEffect(() => {
+    const url =
+      "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setPhotos(data);
+      });
+  }, []);
 
   function toggleCartImage(obj) {
     if (cartItems.some((e) => e.url === obj.url)) {
       setCartItems(prevCartItems => prevCartItems.filter(item => item.url !== obj.url));
       return;
-        }
+    }
 
     setCartItems((prevObjArray) => [...prevObjArray, obj]);
-    }
-    function toggleFavorite(id) {
-        setPhotos(prevState => {
-            return prevState.map(item => {
-                return (item.id === id ? 
-                { ...item,
-                isFavorite: !item.isFavorite } :
-                item)
-            })
-        })
-    }
+  }
+  function toggleFavorite(id) {
+    setPhotos((prevState) => {
+      return prevState.map((item) => {
+        return item.id === id
+          ? { ...item, isFavorite: !item.isFavorite }
+          : item;
+      });
+    });
+  }
   return (
     <UserContext.Provider
       value={{ photos, toggleFavorite, toggleCartImage, cartItems }}
     >
-        {props.children}
+      {props.children}
     </UserContext.Provider>
   );
 }
 
-export {AppContextProvider, UserContext};
+export { AppContextProvider, UserContext };
